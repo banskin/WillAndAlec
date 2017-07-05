f9af1832ab60c13099ad7e2a1c356bbb3aa9f7e2
Banskin <leminwilliam@gmail.com>
2017-07-05 13:38:22 +0100

    Added text to induce seizures

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,29 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <windows.h>
 
+
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello Alec\n";
-    return 0;
+	HANDLE  hConsole;
+	int k;
+
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	// you can loop k higher to see more color choices
+	for (k = 1; k < 255; k++)
+	{
+		// pick the colorattribute k you want
+		SetConsoleTextAttribute(hConsole, k);
+		cout << k << " Hello Alec" << endl;
+	}
+
+	cin.get(); // wait
+	return 0;
 }
+
+
 
