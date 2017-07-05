9d58cc428a28b12c3bd85ebe0d7cb446ed39048a
Banskin <leminwilliam@gmail.com>
2017-07-05 15:58:50 +0100

    empty version

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,5 @@
 
-#include "stdafx.h"
 #include <iostream>
-#include <windows.h>
 
 
 
@@ -9,15 +7,6 @@ using namespace std;
 
 int main()
 {
-	HANDLE  hConsole;
-	int k;
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	for (k = 1; k < 255; k++)
-	{
-		SetConsoleTextAttribute(hConsole, k);
-		cout << k << " Hello Alec" << endl;
-	}
-	cin.get();
 	return 0;
 }
 
